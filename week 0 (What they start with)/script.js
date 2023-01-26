0b8ced0d69dca3841a7cac6f20985b40e978aa67
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-01-25 18:12:46 -0600

    Update script.js

--- week 0 (What they start with)/script.js
+++ week 0 (What they start with)/script.js
@@ -9,4 +9,10 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
 
+var geometry = new THREE.BoxGeometry(1, 1, 1); 
+var material = new THREE.MeshLambertMaterial({color: 0xFFCC00}); 
+var cube1 = new THREE.Mesh(geometry, material);
+cube1.rotation.y = 45;
+scene.add(cube1);
+
 renderer.render(scene, camera)
