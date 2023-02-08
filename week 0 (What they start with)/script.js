2cb00b26148e080e7c1f4af68e84207ebddbb18d
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-02-08 21:45:00 +0000

    light

--- week 0 (What they start with)/script.js
+++ week 0 (What they start with)/script.js
@@ -4,9 +4,19 @@ var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight
 camera.position.z = 5;
 
 var renderer = new THREE.WebGLRenderer({antialias: true})
-renderer.setClearColor("green")
+renderer.setClearColor("blue")
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
+
+var geometry = new THREE.BoxGeometry(1, 1, 1)
+var material = new THREE.MeshLambertMaterial({color: 0xFFCC00})
+var cube1 = new THREE.Mesh(geometry, material)
+cube1.rotation.y = 45; 
+scene.add(cube1)
+
+var light = new THREE.PointLight(new THREE.Color("white"), 1, 500)
+light.position.set(10, 12, 25)
+scene.add(light)
 
 renderer.render(scene, camera)
